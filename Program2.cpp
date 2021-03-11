6fce611f5132c65d5c139f0cf59ebd9b9e8aef62
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-11 16:27:23 +0530

    practice program2

---
+++ Program2.cpp
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Shree
+ *
+ * Created on March 11, 2021, 4:15 PM
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {  
+    int a,b,sum;
+    cout << "enter the value of a:";
+    cin >> a;
+    cout << "enter the value of b:";
+    cin >> b;
+    sum=a+b;
+    cout<<"sum of "<< a << "and"<< b << "is:" << sum;
+    return 0;
+    
+}
+
+
+
+
+
