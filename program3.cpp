649c5b9d53cda4ab2bb02cc792f3c066d270707f
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-11 17:21:26 +0530

    practise program3

---
+++ program3.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Shree
+ *
+ * Created on March 11, 2021, 5:02 PM
+ */
+
+#include <cstdlib>
+#include<iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(){
+
+ cout << "The size of char is:"<< sizeof(char) <<"bytes\n";
+ cout << "The size of short is:"<< sizeof(short) <<"bytes\n";
+ cout << "The size of int is:"<< sizeof(int) <<"bytes\n";
+ cout << "The size of long is:"<< sizeof(long) <<"bytes\n";
+ cout << "The size of long long is:"<< sizeof(long long) <<"bytes\n";
+ cout << "The size of float is:"<< sizeof(float) <<"bytes\n";
+ cout << "The size of double is:"<< sizeof(double) <<"bytes\n";
+ cout << "The size of long double is:"<<sizeof(long double) <<"bytes\n";
+ cout << "The size of bool is:"<< sizeof(bool) <<"bytes\n";
+
+    return 0;
+}
+
