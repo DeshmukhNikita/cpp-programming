3b7032d437738f68a88d5e707b370190e46165d8
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-11 15:54:17 +0530

    1st CPP program

---
+++ program1.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Shree
+ *
+ * Created on March 3, 2021, 12:03 AM
+ */
+
+
+ #include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {  // its a predefine method name in c++ complier. and its a starting point of your  project or class.
+    cout << "Welcome \n";
+    return 0;
+}
+
+
