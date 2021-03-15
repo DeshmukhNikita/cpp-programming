22918baa983851b5e67b8ed9ad2f4c8f03396faa
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-15 16:37:20 +0530

    3rd practise program

---
+++ 3rd practise program.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Shree
+ *
+ * Created on March 14, 2021, 12:39 PM
+8
+ * 8
+ * 7 */
+
+#include <cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+   
+    cout <<" size of integer is: " << sizeof(int) <<" bytes\n";
+    cout <<" size of float is: " << sizeof(float) <<" bytes\n";
+    cout <<" size of char is: " << sizeof(char) <<" bytes\n";
+    cout <<" size of double is: " << sizeof(double) <<" bytes\n";
+
+    return 0;
+}
+
