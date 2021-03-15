33e44d5b1d9e9b86f9d833296a02fbc20c423e42
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-15 16:29:32 +0530

    2st practise program

---
+++ 2st practise program.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Shree
+ *
+ * Created on March 14, 2021, 12:39 PM
+8
+ * 8
+ * 7 */
+
+#include <cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout <" size of integer is :"<< sizeof(int);
+
+    return 0;
+}
+
