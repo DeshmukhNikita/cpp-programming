0e9c5a99e19cea4b07bc569435b83579ea36b0bc
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-15 17:16:47 +0530

    4th practise program

--- 4th practise program.cpp
+++ 4th practise program.cpp
@@ -23,15 +23,8 @@ int main() {
     int a,b;
     cout << " Enter the value of a: ";
     cin >> a;
-    cout << " Enter the value of b: ";
-    cin >> b;
+
     
-    a=a+b;
-    b=a-b;
-    a=a-b;
-          
-  cout << "\n after swapping a is:"<< a;
-  cout << "\n after swapping b is:"<< b;
     
    
     return 0;
