c3e22f13c954e210d3cfa971a08b7efc74532f5d
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-16 19:15:09 +0530

    10th leap year program

---
+++ 10th practise program.cpp
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Shree
+ *
+ * Created on March 14, 2021, 12:39 PM
+8
+ * 8
+ * 7 */
+
+#include <cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+    int year;
+    cout <<" Enter the year: ";
+    cin >> year;
+    
+   if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                cout << year << " is a leap year.";
+            else
+                cout << year << " is not a leap year.";
+        }
+        else
+            cout << year << " is a leap year.";
+    }
+    else
+        cout << year << " is not a leap year.";
+
+    return 0;
+}
+
+
