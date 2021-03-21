53c0150400c684d579d4faefb881ec6e33ebff02
DeshmukhNikita <79835738+DeshmukhNikita@users.noreply.github.com>
2021-03-21 19:27:24 +0530

    17th power of number

---
+++ 17th practise program.cpp
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Shree
+ *
+ * Created on March 14, 2021, 12:39 PM
+8
+ * 8
+ * 7 */
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    
+    cout<<" Enter the value of x&y:";
+    cin >> x >> y;
+    cout<<"x^y:"<< pow(x,y);
+   
+    return 0;
+}
+    
+
